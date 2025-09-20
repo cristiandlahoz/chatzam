18778aff4660fc3b1262c0e452582fe1b35e1c20
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-09-20 12:38:07 -0400

    feat: add ChatZamApplication for Hilt integration
    - Create Application class annotated with @HiltAndroidApp
    - Configure AndroidManifest.xml to use custom Application class
    - Initialize Hilt dependency injection framework

---
+++ app/src/main/java/com/wornux/chatzam/ChatZamApplication.java
@@ -0,0 +1,8 @@
+package com.wornux.chatzam;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ChatZamApplication extends Application {
+}
